5cef4f106b4fe3ee4d0981914bd6f00ac840bac1
Kenny Tian <kenny@Kennys-MacBook-Air.local>
2016-03-06 17:12:09 +0800

    添加首页导航

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LagouApp
 
-开发一个仿拉勾的App.
+### 开发一个仿拉勾的App, 用纯纯的ES6实现.
+
+参考项目:https://github.com/heruijun/react-native-lagou
--- mainpage.js
+++ mainpage.js
@@ -15,17 +15,97 @@ import React,{
 } from 'react-native';
 
 import RouteHome from './routehome';
+/*import Message from  './view/message';
+ import Discover from  './views/discover';
+ import RouteMe from './views/routeme';*/
 
+let HOME_TAB = 'homeTab';
+let MESSAGE_TAB = 'messageTab';
+let DISCOVER_TAB = 'discoverTab';
+let ME_TAB = 'meTab';
 
-class MainPage extends Component {
+export default class MainPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedTab: HOME_TAB,
+            notifCount: 0,
+            presses: 0,
+        };
+    };
+
+    setTab(tabId) {
+        this.setState({selectedTab: tabId});
+    }
+
+    _addNavigator(component, title) {
+        let data = null;
+        if (title === '首页') {
+            data = this.state.data;
+        }
+        return (<NavigatorIOS
+            style={{flex:1}}
+            barTintColor='#FFF'
+            titleTextColor='#666'
+            tintColor='#666'
+            translucent={false}
+            initialRoute={{component:component,title:title,passProps:{data:data}}}
+        />);
+    };
+
+    _renderContent(pageName:string, num:number) {
+        let renderView;
+        switch (pageName) {
+            case HOME_TAB:
+                renderView = <RouteHome />
+                break;
+            case MESSAGE_TAB:
+                //renderView = <Message />
+                break;
+            case DISCOVER_TAB:
+                //renderView = <Discover />
+                break;
+            case ME_TAB:
+                //renderView = <RouteMe />
+                break;
+            default:
+                break;
+        }
+
+        return (<View style={styles.pageView}>{renderView}</View>)
+    };
+
     render() {
         return (
-            <View>
-                <Text>MainPage MainPage MainPage MainPage MainPage MainPage MainPage</Text>
+            <View style={styles.container}>
+                <TabBarIOS
+                    tintColor='#11a984'
+                    barTintColor='#FFF'>
+                    <TabBarIOS.Item
+                        title='首页'
+                        icon={require('./images/icon_home_nor.png')}
+                        selected={this.state.selectedTab === 'HOME_TAB'}
+                        onPress={()=>this.setTab(HOME_TAB)}>
+                        {this._renderContent(HOME_TAB)}
+                    </TabBarIOS.Item>
+                </TabBarIOS>
             </View>
         );
     }
 }
 
-export default MainPage;
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    pageView: {
+        flex: 1,
+    },
+    tabContent: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    tabText: {
+        margin: 50,
+    }
+});
